3fc844adc4b067d355f87b1e8b1bbd37eb427073
darinchakalov <darinchakalov@hotmail.com>
2018-04-08 18:54:36 +0300

    1

--- Exercise: Arrays and Matrices/06. Sort Array.js
+++ Exercise: Arrays and Matrices/06. Sort Array.js
@@ -1,5 +1,6 @@
 function sortArray(arr){
-    console.log(arr.sort((a,b) => a.toLowerCase().length - b.toLowerCase().length).join('\n'));
+    console.log(arr.sort(function(a,b){
+        return a.toLowerCase().localeCompare(b.toLowerCase())}).sort((a,b) => a.length - b.length).join('\n'));
 }
 
-sortArray(['alpha','beta','gamma'])
+sortArray(['test','Deny','omen','Default'])
