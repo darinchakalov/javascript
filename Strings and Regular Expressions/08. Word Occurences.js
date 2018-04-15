db993060deff6cda30842ff28eb51046eb316b34
darin <darin.chakalov@siteground.com>
2018-04-15 05:42:21 +0300

    count occurances

--- Strings and Regular Expressions/08. Word Occurences.js
+++ Strings and Regular Expressions/08. Word Occurences.js
@@ -1,15 +1,25 @@
-function wordOccurences(text, word){
-    let regex = new RegExp("\\b"+word+"\\b", "i")    
-    let count = 0
-    
-    for (const w of text) {
-        let match = regex.exec(w)
-        while (match) {
-            count++
-            match = regex.exec(w)
-        }
+function wordOccurences([string, word]) {
+    var regexExpression ="\\b(" + word + ")\\b";
+    var regex = new RegExp(regexExpression, "ig");
+    let result = []
+    while ( match = regex.exec(string) ){
+        result.push(match);
     }
-    console.log(count);
+    console.log(result.length); 
 }
 
-wordOccurences('The waterfall was so high, that the child couldn’t see its peak.', 'the')
+wordOccurences(['The waterfall was so high, that the child couldn’t see its peak.', 'the'])
+
+
+//second version 
+
+function wordOccurences(string, word) {
+
+    let regex = new RegExp('\\b'+ word + '\\b', 'gi')
+    let res = string.match(regex)
+
+    let result = res == null ? 0 : res.length
+    return result
+}
+
+console.log(wordOccurences('The waterfall was so high, that the child couldn’t see its peak.', 'the'))
