43df4906b2055dacd250b043546b0b90a135a8d9
darin <darin.chakalov@siteground.com>
2018-04-21 23:25:20 +0300

    second version 6th JSON

--- Lab: JavaScript Objects and Assoc Arrays/06. Count Words with Maps.js
+++ Lab: JavaScript Objects and Assoc Arrays/06. Count Words with Maps.js
@@ -18,3 +18,19 @@ function countWordsWithMap (arr){
 }
 
 countWordsWithMap(['Far too slow, you\'re far too slow.'])
+
+
+// second version
+
+function countWordsWithMap (arr){
+    let words = arr.join('\n').toLowerCase().split(/\W+/).filter(s => s != '')
+    let wordCount = new Map()
+    for (let w of words){
+        wordCount.has(w) ? wordCount.set(w, wordCount.get(w) + 1) : wordCount.set(w, 1)
+    }
+    let allWords = Array.from(wordCount.keys()).sort()
+    allWords.forEach(w => console.log(`'${w}' -> ${wordCount.get(w)} times`));
+    
+}
+
+countWordsWithMap(['Far too slow, you\'re far too slow.'])
