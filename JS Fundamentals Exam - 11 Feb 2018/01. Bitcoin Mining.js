09bafce25db534dcc4fcd2f65bfba02a9bef94a0
darin <darin.chakalov@siteground.com>
2018-05-02 19:38:51 +0300

    still the first one

--- JS Fundamentals Exam - 11 Feb 2018/01. Bitcoin Mining.js
+++ JS Fundamentals Exam - 11 Feb 2018/01. Bitcoin Mining.js
@@ -1,27 +1,45 @@
 function bitcoinMining(data){
+    data = data.map(Number)
     let bitcoinsBought = 0
     let daysCount = 1
     let amountOfGold = 0 
     let dollarsInGold = 0
     let dayBoughtOn = 0
+    let costOfBitcoin = 11949.16
+    let canAfford=0;
+    let dollarsLeft = 0
     for (let i = 0; i<data.length; i++){
         if (daysCount == 3){
             data[i] = 0.7* data[i]
         }
         amountOfGold += data[i]
         dollarsInGold = amountOfGold * 67.51
-        dollarsInGold = dollarsInGold.toFixed(2)
-        if (dollarsInGold >= 11949.16){
-            bitcoinsBought++
-            dollarsInGold -= 11949.16
-            amountOfGold = dollarsInGold / 67.51
+        if (dollarsInGold >= costOfBitcoin){
+            canAfford = Math.floor(dollarsInGold / costOfBitcoin)
+            bitcoinsBought+= canAfford
+            dollarsLeft = dollarsInGold - (costOfBitcoin*canAfford)
+            amountOfGold = dollarsLeft / 67.51
+            if (bitcoinsBought >=1 ){
+                dayBoughtOn = daysCount
+            }
+            dollarsInGold = dollarsLeft
         }
-        if (bitcoinsBought >=1 && bitcoinsBought <2){
-            dayBoughtOn = daysCount
-        }
+        
         daysCount++
     }
-    console.log(dayBoughtOn, dollarsInGold, bitcoinsBought);
+    dollarsInGold = dollarsInGold.toFixed(2)
+    dollarsInGold = Number(dollarsInGold)
+
+    if(bitcoinsBought > 0){
+        console.log(`Bought bitcoins: ${bitcoinsBought}`);
+        console.log(`Day of the first purchased bitcoin: ${dayBoughtOn}`);
+        console.log(`Left money: ${dollarsInGold} lv.`);
+    }else{
+        console.log(`Bought bitcoins: ${bitcoinsBought}`);
+        console.log(`Left money: ${dollarsInGold} lv.`);
+    }
 }
 
-bitcoinMining([100, 200, 300])
+bitcoinMining([3124.15,
+    504.212,
+    2511.124])
